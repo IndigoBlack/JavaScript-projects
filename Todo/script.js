fff3c3b794d06dc05b50a2a2c94acbce5adffabf
Mamello Molokwane <161704049+IndigoBlack@users.noreply.github.com>
2025-01-29 22:21:11 +0000

    edit button on Todo list

--- Todo/script.js
+++ Todo/script.js
@@ -15,17 +15,52 @@ function addTask() {
     const taskList = document.querySelector(".task-list");
     const li = document.createElement("li");
     li.innerHTML = `
-        <li><input type="checkbox"> 
-            ${title}<br>
-            ${description}<br>
-            <span>${date}</span><br>
-            <span class="edit-btn">Edit</span><br>
-            <span class="delete=btn">Delete</span><hr>
-        </li>
+        <input type="checkbox"> 
+            <span class="task-title">${title}</span><br>
+            <span class="task-description">${description}</span><br>
+            <span class="task-date">${date}</span><br>
+            <button class="edit-btn">Edit</button><br>
+            <button class="delete=btn">Delete</button><br>
+        
     `;
     taskList.appendChild(li);
+    
+    const editButton = li.querySelector(".edit-btn");
+    editButton.addEventListener("click", function() {
+        const taskTitle = li.querySelector(".task-title");
+        const taskDescription = li.querySelector(".task-description");
+        const taskDate = li.querySelector(".task-date");
+
+        const updateTitle = prompt("Edit title: ", taskTitle.textContent);
+        const updateDescription = prompt("Edit description: ", taskDescription.textContent);
+        taskTitle.textContent = updateTitle;
+        taskDescription.textContent = updateDescription
+        /*
+        if (editButton.textContent === "Edit") {
+            taskTitle.contentEditable = true;
+            taskDescription.contentEditable = true;
+            taskDate.contentEditable = true;
+            editButton.textContent = "Save";
+        } else {
+            taskTitle.contentEditable = false;
+            taskDescription.contentEditable = false;
+            taskDate.contentEditable = false;
+            editButton.textContent = "Edit";
+        }
+        */
+    })
 }
 
-function edit() {
-    
+/*
+function edit(li) {
+    // create an input that will show up when one clicks the edit button.
+    // Get the value of whatever the user typed in the input.
+    // Save the value.
+    const edit = li.querySelector(".edit-btn");
+    const input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = "Enter text here";
+    document.body.appendChild(input);
+    input.style.diplay = "none";
 }
+*/
