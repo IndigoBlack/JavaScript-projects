10a5205be6f2790bcfae2ba06e09b3bc152af14f
Mamello Molokwane <161704049+IndigoBlack@users.noreply.github.com>
2025-02-05 19:06:58 +0000

    modified

--- Todo/script.js
+++ Todo/script.js
@@ -8,28 +8,53 @@
 */
 
 
+// Still have to deal with the checkbox bugs
 function todo() {
-    const title = document.querySelector(".title").value;
-    const description = document.querySelector(".description").value;
-    const date = document.querySelector(".date").value;
+    const title = document.querySelector("#title").value;
+    const description = document.querySelector("#description").value;
+    const date = document.querySelector("#date").value;
     const taskList = document.querySelector(".task-list");
     const li = document.createElement("li");
     li.innerHTML = `
-        <input type="checkbox"> 
+        <input type="checkbox" class="form-check-input"> 
             <span class="task-title">${title}</span><br>
             <span class="task-description">${description}</span><br>
             <span class="task-date">${date}</span><br>
-            <button class="edit-btn">Edit</button><br>
-            <button class="delete-btn">Delete</button><br>
+            <button id="edit-btn" class="btn btn-secondary">Edit</button><br>
+            <button id="delete-btn" class="btn btn-secondary">Delete</button><br>
         
     `;
     taskList.appendChild(li);
+    const taskTitle = li.querySelector(".task-title");
+    const taskDescription = li.querySelector(".task-description");
+    const taskDate = li.querySelector(".task-date");
+
+    const check = [];
+    const checkbox = document.querySelector(".form-check-input");
+    const checkedTasks = document.querySelector(".checked-tasks");
+    console.log(checkbox.checked)
+    const completed = () => {
+        console.log("Checkbox clicked");
+        if (checkbox.checked) {
+            console.log("Checkbox checked.");
+            check.push(taskTitle.textContent);
+            console.log("It should do now.")
+        }
+        
+        for (let i = 0; i < check.length; i++) {
+            if (!checkbox.checked) {
+                check.splice(i, 1);
+                i--;
+            }
+        }
+        let items = check.map(item => `<li>${item}</li>`)
+        checkedTasks.innerHTML = items.join(" ");
+    }
+    checkbox.addEventListener("click", completed);
     
-    const editButton = li.querySelector(".edit-btn");
+
+    const editButton = li.querySelector("#edit-btn");
     editButton.addEventListener("click", function() {
-        const taskTitle = li.querySelector(".task-title");
-        const taskDescription = li.querySelector(".task-description");
-        const taskDate = li.querySelector(".task-date");
 
         const updateTitle = prompt("Edit title: ", taskTitle.textContent);
         const updateDescription = prompt("Edit description: ", taskDescription.textContent);
@@ -50,7 +75,7 @@ function todo() {
         */
     });
     
-    const deleteButton = li.querySelector(".delete-btn");
+    const deleteButton = li.querySelector("#delete-btn");
     deleteButton.addEventListener("click", function() {
         taskList.remove(li);
     });
