ccc827d3b3c4b8030f3b47dfc6c21d13751421b5
Mamello Molokwane <161704049+IndigoBlack@users.noreply.github.com>
2025-02-09 09:40:41 +0000

    Writing psuedocode to outline the project

---
+++ Flashcards/script.js
@@ -0,0 +1,14 @@
+// 6. Flashcards App
+//     Description: A study app where users can create flashcards for learning.
+//     Features:
+//     Create flashcards with a question and answer.
+//     Shuffle and display flashcards randomly.
+//     Track progress and mark cards as "known" or "unknown."
+
+// Function for creating a flashcard With a question and answer. Function will be placed on the submit button.
+// Function for displaying the answer the button is clicked.
+// Function for marking the card as known or unkown (option: You can make it 1 function or 2).
+// Function for next card.
+// Function to shuffle and display the cards.
+// Function to show the number of known and unknown cards.
+
